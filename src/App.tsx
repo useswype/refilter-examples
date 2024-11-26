27ae37654ffeed53a0022070b6feca0ef7839b0f
Mahmoud Adel Mohamed <36308551+mahmoudadelmohamed@users.noreply.github.com>
2024-11-26 05:03:58 +0200

    example: fix table footer with elements

--- src/App.tsx
+++ src/App.tsx
@@ -45,46 +45,50 @@ function App() {
           return true;
         }}
       />
-      <div className="relative mt-4 overflow-auto h-[calc(100vh-10rem)]  shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left">
-          <thead className="text-xs uppercase sticky top-0 z-50 bg-white w-full ">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Category
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Date
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price (EGP)
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, i) => (
-              <tr key={i} className="odd:bg-gray-50 even:bg-white border-b">
-                <td
-                  scope="row"
-                  className="px-6 py-4 font-medium whitespace-nowrap"
-                >
-                  {item.productName}
-                </td>
-                <td className="px-6 py-4">{item.category}</td>
-                <td className="px-6 py-4">{format(item.date, 'dd-MM-yyyy')}</td>
-                <td className="px-6 py-4">{item.price}</td>
+      <div className="relative mt-4 overflow-auto h-[calc(100vh-10rem)] shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left h-full">
+            <thead className="text-xs uppercase sticky top-0 z-50 bg-white w-full">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Category
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Date
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price (EGP)
+                </th>
               </tr>
-            ))}
-          </tbody>
+            </thead>
+            <tbody>
+              {data.map((item, i) => (
+                <tr key={i} className="odd:bg-gray-50 even:bg-white border-b h-2">
+                  <td
+                    scope="row"
+                    className="px-6 py-4 font-medium whitespace-nowrap"
+                  >
+                    {item.productName}
+                  </td>
+                  <td className="px-6 py-4">{item.category}</td>
+                  <td className="px-6 py-4">
+                    {format(item.date, 'dd-MM-yyyy')}
+                  </td>
+                  <td className="px-6 py-4">{item.price}</td>
+                </tr>
+              ))}
+            </tbody>
           <tfoot className="sticky bottom-0 bg-gray-100 w-full">
-            <td></td>
-            <td></td>
-            <td scope="row" className="px-6 py-3 text-base">
-              Total Rows
-            </td>
-            <td className="px-6 py-3">{data.length}</td>
+            <tr>
+              <td></td>
+              <td></td>
+              <td scope="row" className="px-6 py-3 text-base">
+                Total Rows
+              </td>
+              <td className="px-6 py-3">{data.length}</td>
+            </tr>
           </tfoot>
         </table>
       </div>
